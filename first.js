a315f799754c648428c053abd47ead2f41defcac
NAJEEBKHAN123 <145932116+NAJEEBKHAN123@users.noreply.github.com>
2023-12-08 23:14:49 +0500

    add

---
+++ first.js
@@ -0,0 +1,165 @@
+
+// Arithematic operations
+// let a = 4;
+// let b = 2;
+// let c = a + b;
+// console.log("a - b=", a - b);
+// console.log("a * b=", a * b);
+// console.log("a / b=", a / b);
+// console.log("a + b=", a + b);
+// console.log("a % b=", a % b);
+// console.log("a ** b=", a ** b);
+
+// Unary Operator
+// let a = 5;
+// let b = 4;
+// console.log("a =", a, "& b = ,", b);
+// a = a-1;
+// console.log("--a", --a,);
+// console.log("a =", a);
+
+// a = 5;
+// b = 4;
+// a *=3;
+// console.log("a =", a);
+// a = 5;
+// b = 4;
+// a +=3;
+// console.log("a =", a);
+// a = 5;
+// b = 4;
+// a %=3;
+// console.log("a =", a);
+// a = 5;
+// b = 4;
+// a **=3;
+// console.log("a =", a);
+
+
+// conparison operator
+// a = 5;
+// b = 4;
+// console.log("5 >= 4", a >= b);
+//logical o
+// a = 5;
+// b = 4;
+// let cond1 = a > b;
+// let cond2 = a ===5;
+
+// console.log("cond1 && cond2 =", cond1 && cond2);
+
+// logical OR ||
+// a = 5;
+// b = 4;
+// let cond1 = a > b;
+// let cond2 = a === 5;
+
+// console.log("cond1 || cond2 =", a < b || a === 5);
+
+// logical not
+// a = 5;
+// b = 4;
+// console.log(" !(5>4)=", !(a > b));
+
+// conditional statements
+// let color;
+// if(mode === "dark-mode"){
+//     color = "black";
+// }
+// let age = 10;
+// if( age >= 10){
+//     console.log( "you can vote");
+// }
+// if( age < 10){
+//     console.log( "you can't vote");
+// }
+// let mode = "light";
+// let color;
+// if(mode === "dark"){
+//     color = "black";
+// }
+// else{
+//     color = "white"
+// }
+
+// console.log(light);
+
+
+
+// odd or even
+// let num = 34563;
+// if(num%2 === 0){
+//     console.log(num, "is even");
+// } else{
+//     console.log(num, "is odd");
+// }
+
+// syntax -> rules of conditional statements
+
+// if-else statemtns
+// let age = 40;
+// if(age < 18){
+//     console.log("underage");
+// } else if (age > 30){
+//     console.log("overage");
+// }else{
+//     console.log("middle");
+// }
+// let mode = "red";
+// let color;
+// if(mode === "dark"){
+//     color ="black";
+// }else if(mode === "blue")
+// {color = "blue";
+
+// }else if(moode === "pink"){
+//     color = "aqua";
+
+// }else{
+//     color = "yellow";
+// }
+// console.log(color)
+
+
+// ternary operator
+// let age = 12;
+// let result = age >= 18 ? "adult" : "not adult";
+// console.log(result)
+// function testNum(a) {
+//   let result;
+//   if (a > 0) {
+//     result = 'positive';
+//   } else {
+//     result = 'NOT positive';
+//   }
+//   return result;
+// }
+
+// console.log(testNum(1));
+// let name = prompt("hello!");
+// console.log(name);
+
+//   let num = prompt("enter a number");
+//   if(num % 5 === 0){
+//     console.log("num is multiple of 5")
+//   }
+//   else{
+//     console.log(num, "is Not a mutiple of 5")
+//   }
+// parctice 2
+// let score = prompt("enter your score (1-100):");
+// let grade;
+// if(score >= 90 && score <= 100){
+//     grade = "A";
+// } else if(score >= 70 && score <=89){
+//     grade = "B"
+// }else if(score >= 60 && score <= 69){
+//     grade = "C"
+// }else if(score >= 50 && score <= 59){
+//     grade = "D"
+// }else if(score >= 40 && score <= 49){
+//     grade = "E"
+// }else{
+//     grade = "F"
+// }
+// console.log("according to your score your grade:",grade;)
